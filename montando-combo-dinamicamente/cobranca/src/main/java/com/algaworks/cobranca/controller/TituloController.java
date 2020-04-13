e57cfa5d0ac78da28692186b9e957b26c5c43a46
Thiago <tvttavares@gmail.com>
2020-04-13 09:14:36 -0400

    Montando o combo dinamicamente

--- montando-combo-dinamicamente/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ montando-combo-dinamicamente/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -12,32 +12,33 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
-import com.algaworks.cobranca.repository.TitulosRepository;
+import com.algaworks.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-
+	
 	@Autowired
-	private TitulosRepository titulos;
+	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		return mv;
 	}
-
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
+		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
-
 		return mv;
 	}
-
+	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
+	
 }
--- montando-combo-dinamicamente/cobranca/src/main/java/com/algaworks/cobranca/model/StatusTitulo.java
+++ montando-combo-dinamicamente/cobranca/src/main/java/com/algaworks/cobranca/model/StatusTitulo.java
@@ -2,16 +2,17 @@ package com.algaworks.cobranca.model;
 
 public enum StatusTitulo {
 
-	PENDENTE("Pendente"), RECEBIDO("Recebido") ;
-
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
 	private String descricao;
-
+	
 	StatusTitulo(String descricao) {
 		this.descricao = descricao;
 	}
-
+	
 	public String getDescricao() {
 		return descricao;
 	}
-
+	
 }
