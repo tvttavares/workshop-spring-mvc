f56ea7c93edcc32419b6d9021f4f0885f3e3aa2d
Thiago <tvttavares@gmail.com>
2020-04-12 20:05:13 -0400

    Mostrando a mensagem de sucesso

--- mostrando-mensagem-sucesso/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ mostrando-mensagem-sucesso/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -7,27 +7,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
-import com.algaworks.cobranca.repository.TitulosRepository;
+import com.algaworks.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-
+	
 	@Autowired
-	private TitulosRepository titulos;
+	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
 	}
-
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
+		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
-
 		return mv;
 	}
-
+	
 }
