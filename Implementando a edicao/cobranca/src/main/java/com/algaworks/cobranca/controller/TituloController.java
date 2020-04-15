8a860a37b5c85356a0eb024b4d3861551e6494e6
thiago <tvttavares@gmail.com>
2020-04-14 20:48:21 -0400

    Implementando a edição

--- Implementando a edicao/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ Implementando a edicao/cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -16,56 +16,53 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
-import com.algaworks.cobranca.repository.TitulosRepository;
+import com.algaworks.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-
+	
 	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
 	@Autowired
-	private TitulosRepository titulos;
+	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
-
 		return mv;
 	}
-
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
 			return CADASTRO_VIEW;
 		}
-
+		
 		titulos.save(titulo);
 		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
-
-		return "redirect:/titulos/novo";
+		return "redirect:/titulos/novo"; 
 	}
-
+	
 	@RequestMapping
 	public ModelAndView pesquisar() {
 		List<Titulo> todosTitulos = titulos.findAll();
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
-
 		return mv;
 	}
-
+	
 	@RequestMapping("{codigo}")
 	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
-//		Titulo titulo = titulos.findById(codigo).get();
-		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW); 
 		mv.addObject(titulo);
-
 		return mv;
 	}
-
+	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
+	
 }
