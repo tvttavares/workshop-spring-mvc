74871ff83eee2cb09d151ed12f3cee542c719851
Thiago <tvttavares@gmail.com>
2020-04-16 20:44:38 -0400

    Context path no Tomcat externo

--- context-path-no-tomcat-externo/cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ context-path-no-tomcat-externo/cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -17,19 +17,20 @@ public class CobrancaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CobrancaApplication.class, args);
 	}
-
+	
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
-
+	
 	@Configuration
 	public static class MvcConfig extends WebMvcConfigurerAdapter {
-
+		
 		@Override
 		public void addViewControllers(ViewControllerRegistry registry) {
 			registry.addRedirectViewController("/", "/titulos");
 		}
-
+		
 	}
+	
 }
